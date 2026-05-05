1fa086fbfe9b9c95d45e7fc0d60e64c57cc5fee7
Muhammad Zubair <zubairshafiqueskp686@gmail.com>
2026-05-05 17:12:02 +0500

    add all js practice files

--- Js11.js
+++ Js11.js
@@ -10,7 +10,7 @@ cart.push("Keyboard");
 // User removes last item
 cart.pop();
 
-console.log("Cart:", cart.join(", "));
+// console.log("Cart:", cart.join(", "));
 
 // Check if item exists before adding
 if (!cart.includes("Laptop")) {
